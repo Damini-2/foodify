09389f986261ec0a2d331b8c44be2ed1d10a424c
Damini <93993323+Damini-2@users.noreply.github.com>
2023-07-05 15:41:43 +0530

    v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { BookComponent } from './book/book.component';
 import { MenuComponent } from './menu/menu.component';
 
 const routes: Routes = [
-  {path:"foodify/foodify/",component:HomeComponent},
+  {path:"/foodify",component:HomeComponent},
   {path:"about",component:AboutComponent},
   {path:"book",component:BookComponent},
   {path:"menu",component:MenuComponent}
